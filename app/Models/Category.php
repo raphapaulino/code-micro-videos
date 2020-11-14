fcd6e2b36cad141779b6adc82bd70ae0a02d58f2
Raphael Paulino <raphael.paulino.web@gmail.com>
2020-11-14 19:34:09 -0300

    - Substituindo ID Por UUID

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Traits\Uuid;
 
     protected $fillable = ['name', 'description', 'is_active'];
 
     protected $dates = ['deleted_at'];
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
 }
